8057a317e1986f6bce1febcb9b8b45f60f0dc76d
sfinley89 <sfinley@hovelstay.com>
2015-10-29 15:41:11 -0700

    committed

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,13 @@
+class ApplicationController < ActionController::Base
+  # Prevent CSRF attacks by raising an exception.
+  # For APIs, you may want to use :null_session instead.
+
+  protected
+    def authenticate
+      authenticate_or_request_with_http_basic do |username, password|
+      username == "admin" && password == "test"
+    end
+  end
+  
+  protect_from_forgery with: :exception
+end
---
+++ app/controllers/welcome_controller.rb
@@ -0,0 +1,27 @@
+class WelcomeController < ApplicationController
+	before_filter :authenticate
+  
+  def index
+  	@ask 
+  end
+
+  def ask
+  end
+
+  
+
+  def yoda
+  	#@voice = HTTParty.get('https://yoda.p.mashape.com/yoda?sentence=You+will+learn+how+to+speak+like+me+someday.++Oh+wait.').parsed_response
+  	
+  	#sentence = "You+will+learn+how+to+speak+like+me+someday.++Oh+wait."
+  	sentence = params[:sentence]
+  	#render plain: ENV["YODA_KEY"]
+
+ 	@response = Unirest.get "https://yoda.p.mashape.com/yoda?sentence="+sentence,
+ 		headers:{
+   		"X-Mashape-Key" => ENV["YODA_KEY"],
+   		"Accept" => "text/plain",
+  		}
+  end
+  
+end
