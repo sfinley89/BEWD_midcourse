8471e85bf4841f16d1f3c96f4dd4224a80925763
sfinley89 <sfinley@hovelstay.com>
2015-10-31 15:17:20 -0700

    remove htaccess

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,13 +2,7 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
 
-# FOR HTACCESS 
-  # protected
-  #   def authenticate
-  #     authenticate_or_request_with_http_basic do |username, password|
-  #     username == "admin" && password == "test"
-  #   end
-  # end
+
   
   protect_from_forgery with: :exception
 end
--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,5 @@
 class WelcomeController < ApplicationController
-	before_filter :authenticate
+	
   
   def index
   	@ask 
